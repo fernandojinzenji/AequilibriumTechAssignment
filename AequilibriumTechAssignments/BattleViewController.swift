46ca01296c71ffb26ca0ef76a68675ecdeacf53e
Fernando Jinzenji <fernandoj@me.com>
2017-08-15 02:21:12 -0700

    added final enhancements

--- AequilibriumTechAssignments/BattleViewController.swift
+++ AequilibriumTechAssignments/BattleViewController.swift
@@ -11,9 +11,14 @@ import UIKit
 class BattleViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var navigationBar: UINavigationBar!
+    @IBOutlet weak var survivorsButton: UIButton!
+    @IBOutlet weak var redView: UIView!
     
     var selectedAutobots = [Transformer]()
     var selectedDecepticons = [Transformer]()
+    
+    var defeatedAutobots = [Transformer]()
+    var defeatedDecepticons = [Transformer]()
     
     var numberOfBattles = 0
     var autobotWinCount = 0
@@ -26,73 +31,9 @@ class BattleViewController: UIViewController, UITableViewDataSource, UITableView
         self.selectedAutobots = sortByRank(array: self.selectedAutobots)
         self.selectedDecepticons = sortByRank(array: self.selectedDecepticons)
         
-//        self.selectedAutobots.removeAll()
-//        self.selectedDecepticons.removeAll()
-//        self.selectedAutobots.append(Transformer(pName: "auto", pType: .Autobot, pStrength: 5, pIntelligence: 5, pSpeed: 5, pEndurance: 5, pRank: 5, pCourage: 5, pFirepower: 5, pSkill: 5))
-//        self.selectedDecepticons.append(Transformer(pName: "dece", pType: .Decepticon, pStrength: 5, pIntelligence: 5, pSpeed: 5, pEndurance: 5, pRank: 5, pCourage: 5, pFirepower: 5, pSkill: 5))
-        
-        
         // Get number of battles (biggest array)
         self.numberOfBattles = (self.selectedAutobots.count < self.selectedDecepticons.count) ? self.selectedAutobots.count : self.selectedDecepticons.count
-        
-//        // Fight robots
-//        for i in 0 ..< self.numberOfBattles {
-//            
-//            let autobot = self.selectedAutobots[i]
-//            let decepticon = self.selectedDecepticons[i]
-//            
-//            // SPECIAL RULE: if Optimus Prime encounter Predaking, the game ends immediately. Otherwise, both wins against everyone else
-//            if autobot.name == "Optimus Prime" && decepticon.name == "Predaking" {
-//                print("clash")
-//                break;
-//            }
-//            else if autobot.name == "Optimus Prime" {
-//                self.autobotWins += 1
-//                break;
-//            }
-//            else if decepticon.name == "Predaking" {
-//                self.decepticonWins += 1
-//                break;
-//            }
-//            
-//            // RULE 1: If robot is stronger (3 or more strength points) but coward (4 or less courage points), oponent wins
-//            if autobot.strength - decepticon.strength >= 3 && decepticon.courage - autobot.courage >= 4 {
-//                self.decepticonWins += 1
-//                break;
-//            }
-//            else if decepticon.strength - autobot.strength >= 3 && autobot.courage - decepticon.courage >= 4 {
-//                self.autobotWins += 1
-//                break;
-//            }
-//            
-//            // RULE 2: If robot is 3 or more points of skill, it wins the fight
-//            if autobot.skill - decepticon.skill >= 3 {
-//                self.autobotWins += 1
-//                break;
-//            }
-//            else if decepticon.skill - autobot.skill >= 3 {
-//                self.decepticonWins += 1
-//                break;
-//            }
-//            
-//            // RULE 3: Winner is the robot with bigger overall rating
-//            if autobot.overallRating > decepticon.overallRating {
-//                self.autobotWins += 1
-//            }
-//            else if decepticon.overallRating > autobot.overallRating {
-//                self.decepticonWins += 1
-//            }
-//        }
-        
-//        if self.autobotWins > self.decepticonWins {
-//            self.navigationBar.items?[0].title = "Autobots Win!"
-//        }
-//        else if self.decepticonWins > self.autobotWins {
-//            self.navigationBar.items?[0].title = "Decepticon Win!"
-//        }
-//        else {
-//            self.navigationBar.items?[0].title = "Tie!"
-//        }
+
     }
     
     // MARK: Actions
@@ -111,6 +52,32 @@ class BattleViewController: UIViewController, UITableViewDataSource, UITableView
         
     }
     
+    @IBAction func showSurvivorsButtonPressed(_ sender: UIButton) {
+        
+        var detail = ""
+        var survivors = [Transformer]()
+        
+        // Set alert title and filter survivors
+        if self.autobotWinCount > self.decepticonWinCount {
+            survivors = filterSurvivors(allArray: self.selectedDecepticons, defeatedArray: self.defeatedDecepticons)
+        }
+        else {
+            survivors = filterSurvivors(allArray: self.selectedAutobots, defeatedArray: self.defeatedAutobots)
+        }
+        
+        // Loop through survivors
+        for transformer in survivors {
+            
+            detail += "- \(transformer.name)\n"
+            
+        }
+        
+        let alert = UIAlertController(title: "Survivors", message: detail, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Close", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "battle-cell") as! BattleTableViewCell
@@ -126,17 +93,20 @@ class BattleViewController: UIViewController, UITableViewDataSource, UITableView
         let battle = Battle(pAutobot: autobot, pDecepticon: decepticon)
         battle.fight()
         
-        // Show trophy to winner
+        // Show trophy to winner, increment counter and send defeated to respective array
         switch battle.winner {
+        case .clash:
+            self.redView.isHidden = false
         case .autobot:
             cell.autobotTrophy.isHidden = false
             cell.decepticonTrophy.isHidden = true
+            self.defeatedDecepticons.append(decepticon)
+            self.autobotWinCount += 1
         case .decepticon:
             cell.autobotTrophy.isHidden = true
             cell.decepticonTrophy.isHidden = false
-        case .tie:
-            cell.autobotTrophy.isHidden = true
-            cell.decepticonTrophy.isHidden = true
+            self.defeatedAutobots.append(autobot)
+            self.decepticonWinCount += 1
         default:
             cell.autobotTrophy.isHidden = true
             cell.decepticonTrophy.isHidden = true
@@ -159,6 +129,20 @@ class BattleViewController: UIViewController, UITableViewDataSource, UITableView
             cell.battleDetail.text! += "N/A"
         }
         
+        // Update winning team
+        if self.autobotWinCount > self.decepticonWinCount {
+            self.navigationBar.items?[0].title = "Autobots Win 😀"
+            self.survivorsButton.isHidden = false
+        }
+        else if self.decepticonWinCount > self.autobotWinCount {
+            self.navigationBar.items?[0].title = "Decepticons Win 😕"
+            self.survivorsButton.isHidden = false
+        }
+        else {
+            self.navigationBar.items?[0].title = "Tie match!!"
+            self.survivorsButton.isHidden = true
+        }
+        
         return cell
     }
     
@@ -168,6 +152,16 @@ class BattleViewController: UIViewController, UITableViewDataSource, UITableView
         
         return array.sorted(by: { (t1, t2) -> Bool in
             return t1.rank < t2.rank
+        })
+        
+    }
+    
+    func filterSurvivors(allArray: [Transformer], defeatedArray: [Transformer]) -> [Transformer] {
+        
+        return allArray.filter({ (t) -> Bool in
+            !defeatedArray.contains(where: { (t1) -> Bool in
+                return t1.name == t.name
+            })
         })
         
     }
